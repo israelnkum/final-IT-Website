d49e0eb40b312b416d5420669902e6f4b743fb91
root <israelnkum@gmail.com>
2018-09-28 11:26:06 +0000

    First Commit

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -15,42 +15,42 @@
     <link href="{{'storage/images/favicon.png'}}" rel="shortcut icon" type="image/png">
 <link rel="icon" href="{{'storage/images/favicon.png'}}">
     <!-- Stylesheet -->
-    <link href="{{'css/bootstrap.min.css'}}" rel="stylesheet" type="text/css">
-    <link href="{{'css/jquery-ui.min.css'}}" rel="stylesheet" type="text/css">
-    <link href="{{'css/animate.css'}}" rel="stylesheet" type="text/css">
-    <link href="{{'css/css-plugin-collections.css'}}" rel="stylesheet"/>
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/jquery-ui.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/animate.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/css-plugin-collections.css')}}" rel="stylesheet"/>
     <!-- CSS | menuzord megamenu skins -->
-    <link href="{{'css/menuzord-megamenu.css'}}" rel="stylesheet"/>
-    <link id="menuzord-menu-skins" href="{{'css/menuzord-skins/menuzord-rounded-boxed.css'}}" rel="stylesheet"/>
+    <link href="{{asset('css/menuzord-megamenu.css')}}" rel="stylesheet"/>
+    <link id="menuzord-menu-skins" href="{{asset('css/menuzord-skins/menuzord-rounded-boxed.css')}}" rel="stylesheet"/>
     <!-- CSS | Main style file -->
-    <link href="{{'css/style-main.css'}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/style-main.css')}}" rel="stylesheet" type="text/css">
     <!-- CSS | Preloader Styles -->
-    <link href="{{'css/preloader.css'}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/preloader.css')}}" rel="stylesheet" type="text/css">
     <!-- CSS | Custom Margin Padding Collection -->
-    <link href="{{'css/custom-bootstrap-margin-padding.css'}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/custom-bootstrap-margin-padding.css')}}" rel="stylesheet" type="text/css">
     <!-- CSS | Responsive media queries -->
-    <link href="{{'css/responsive.css'}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/responsive.css')}}" rel="stylesheet" type="text/css">
     <!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
     <!-- <link href="css/style.css" rel="stylesheet" type="text/css"> -->
 
     <!-- Revolution Slider 5.x CSS settings -->
-    <link  href="{{'js/revolution-slider/css/settings.css'}}" rel="stylesheet" type="text/css"/>
-    <link  href="{{'js/revolution-slider/css/layers.css'}}" rel="stylesheet" type="text/css"/>
-    <link  href="{{'js/revolution-slider/css/navigation.css'}}" rel="stylesheet" type="text/css"/>
+    <link  href="{{asset('js/revolution-slider/css/settings.css')}}" rel="stylesheet" type="text/css"/>
+    <link  href="{{asset('js/revolution-slider/css/layers.css')}}" rel="stylesheet" type="text/css"/>
+    <link  href="{{asset('js/revolution-slider/css/navigation.css')}}" rel="stylesheet" type="text/css"/>
 
     <!-- CSS | Theme Color -->
-    <link href="{{'css/colors/theme-skin-color-set3.css'}}" rel="stylesheet" type="text/css">
+    <link href="{{asset('css/colors/theme-skin-color-set3.css')}}" rel="stylesheet" type="text/css">
 
     <!-- external javascripts -->
-    <script src="{{'js/jquery-2.2.4.min.js'}}"></script>
-    <script src="{{'js/jquery-ui.min.js'}}"></script>
-    <script src="{{'js/bootstrap.min.js'}}"></script>
+    <script src="{{asset('js/jquery-2.2.4.min.js')}}"></script>
+    <script src="{{asset('js/jquery-ui.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
     <!-- JS | jquery plugin collection for this theme -->
-    <script src="{{'js/jquery-plugin-collection.js'}}"></script>
+    <script src="{{asset('js/jquery-plugin-collection.js')}}"></script>
 
     <!-- Revolution Slider 5.x SCRIPTS -->
-    <script src="{{'js/revolution-slider/js/jquery.themepunch.tools.min.js'}}"></script>
-    <script src="{{'js/revolution-slider/js/jquery.themepunch.revolution.min.js'}}"></script>
+    <script src="{{asset('js/revolution-slider/js/jquery.themepunch.tools.min.js')}}"></script>
+    <script src="{{asset('js/revolution-slider/js/jquery.themepunch.revolution.min.js')}}"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
